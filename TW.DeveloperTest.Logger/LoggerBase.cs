83e7d2d4895f2bab27d338772c0abd548e1a2885
Paul Shpilsher <paul.shpilsher@gmail.com>
2022-12-09 17:46:50 +0300

    add logger base abstract

---
+++ TW.DeveloperTest.Logger/LoggerBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TW.DeveloperTest.Logger
+{
+    public abstract class LoggerBase : ILogger
+    {
+        public void Error(string message)
+        {
+            Log(LogLevel.Error, message);
+        }
+
+        public void Info(string message)
+        {
+            Log(LogLevel.Info, message);
+        }
+
+        public void Warning(string message)
+        {
+            Log(LogLevel.Warning, message);
+        }
+
+
+        public abstract void Log(LogLevel logLevel, string message);
+    }
+}
